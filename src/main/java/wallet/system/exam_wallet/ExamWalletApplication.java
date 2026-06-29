70d5980add6dcb7e24a1476adb4ed4099d60c30e
Hadja Bah <hadja-oumou.bah@ism.edu.sn>
2026-06-29 09:21:27 +0000

    initial project structure

---
+++ src/main/java/wallet/system/exam_wallet/ExamWalletApplication.java
@@ -0,0 +1,13 @@
+package wallet.system.exam_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamWalletApplication.class, args);
+	}
+
+}
